d14a2020be873b51e6ccb89ab5ed743c437cb2b2
tikoot <tiko.otarashvili99@gmail.com>
2022-10-21 13:03:22 +0200

    feat:add welcome page

---
+++ src/pages/WelcomePage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div
+    class="flex flex-col items-center m-auto font-contractica font-bold text-3xl"
+  >
+    <div><img src="../assets/Vector.png" alt="" /></div>
+    <router-link
+      id="linki"
+      to="/identify"
+      class="= flex flex-col text-center pt-[101px] hover:outline-[#243c5a]"
+    >
+      <span class="pb-2 text-center">კითხვარის</span>
+      <span>დაწყება</span>
+    </router-link>
+  </div>
+</template>
