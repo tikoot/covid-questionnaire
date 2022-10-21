b78d9e85ac7020aed8a442300b4959de1263075c
tikoot <tiko.otarashvili99@gmail.com>
2022-10-21 13:00:48 +0200

    feat:add tailwind configuration with custom fotns

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        bpgArial: ["BPG Arial"],
+        contractica: ["TBC Contractica CAPS"],
+      },
+    },
   },
   plugins: [],
 };
