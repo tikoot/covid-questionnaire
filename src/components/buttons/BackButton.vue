0f9dc85e97e0c7fe6374108e0483bb4b98cd9d20
tikoot <tiko.otarashvili99@gmail.com>
2022-10-28 12:16:09 +0200

    feat:add props back button

--- src/components/buttons/BackButton.vue
+++ src/components/buttons/BackButton.vue
@@ -1,7 +1,17 @@
 <template>
-  <section class="">
-    <router-link to="/identify">
-      <img src="@/assets/images/Vector 7.png" />
-    </router-link>
-  </section>
+  <router-link :to="to">
+    <img src="@/assets/images/Vector 7.png" class=""
+  /></router-link>
 </template>
+
+<script>
+export default {
+  props: {
+    to: {
+      type: String,
+      required: false,
+      default: "/",
+    },
+  },
+};
+</script>
