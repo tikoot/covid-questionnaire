64858bcdccb7839ce826fc3829779880cd7890d6
tikoot <tiko.otarashvili99@gmail.com>
2022-10-29 23:37:14 +0200

    feat:create vaccine module

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,13 @@ import { createStore } from "vuex";
 
 import userModule from "./indetifyUser/index.js";
 import situationModule from "./covidSituation/index.js";
+import vaccineModule from "./covidVaccine/index.js";
 
 const store = createStore({
   modules: {
     userModule,
     situationModule,
+    vaccineModule,
   },
   state() {
     return {};
