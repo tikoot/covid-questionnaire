03f8804b38890786acc352eda7dc227a65224b5b
tikoot <tiko.otarashvili99@gmail.com>
2022-10-30 01:21:44 +0200

    feat: add forms which appear according answers

--- src/pages/questionnaire/CovidVaccine.vue
+++ src/pages/questionnaire/CovidVaccine.vue
@@ -2,7 +2,7 @@
   <the-wrapper>
     <the-header page="2"></the-header>
     <section>
-      <VueForm @submit="onSubmit" v-slot="{ meta }">
+      <VueForm @submit="onSubmit" v-slot="{ meta, values }">
         <div class="flex justify-between">
           <div class="min-w-[35%] pt-[42px]">
             <h2 class="text-[22px] font-bold pb-[8px]">უკვე აცრილი ხარ?*</h2>
@@ -29,7 +29,128 @@
               name="had_vaccine"
               class="text-[#F15524] text-base pt-[5px] pl-5"
             />
+            <div v-if="values.had_vaccine === 'true'" class="pt-[46px]">
+              <h2 class="text-[22px] font-bold pb-[8px]">
+                აირჩიე რა ეტაპზე ხარ*
+              </h2>
+              <div class="pl-5 pb-[16px]">
+                <Field
+                  name="vaccine_stage"
+                  type="radio"
+                  value="first_dosage_and_registered_on_the_second"
+                  rules="required"
+                  v-model="vaccine_stage"
+                />
+                <label class="text-[20px] pl-[19px]"
+                  >პირველი დოზა და დარეგისტრირებული ვარ მეორეზე</label
+                >
+              </div>
+              <div class="pl-5 pb-[16px]">
+                <Field
+                  name="vaccine_stage"
+                  type="radio"
+                  value="fully_vaccinated"
+                  v-model="vaccine_stage"
+                />
+                <label class="text-[20px] pl-[19px]">სრულად აცრილი ვარ</label>
+              </div>
+              <div class="pl-5">
+                <Field
+                  name="vaccine_stage"
+                  type="radio"
+                  value="first_dosage_and_not_registered_yet"
+                  v-model="vaccine_stage"
+                />
+                <label class="text-[20px] pl-[19px]"
+                  >პირველი დოზა და არ დავრეგისტრირებულვარ მეორეზე</label
+                >
+              </div>
+              <ErrorMessage
+                name="vaccine_stage"
+                class="text-[#F15524] text-base pt-[5px] pl-5"
+              />
+            </div>
+            <div
+              v-if="
+                values.vaccine_stage === 'first_dosage_and_not_registered_yet'
+              "
+              class="pt-[39px] pl-[62px] text-xl"
+            >
+              <p>
+                რომ არ გადადო, <br />
+                ბარემ ახლავე დარეგისტრირდი <br />
+                <a
+                  href="https://booking.moh.gov.ge/"
+                  target="_blank"
+                  class="text-[#1289AE]"
+                >
+                  https://booking.moh.gov.ge/</a
+                >
+              </p>
+            </div>
+            <div v-if="values.had_vaccine === 'false'" class="pt-[46px]">
+              <h2 class="text-[22px] font-bold pb-[8px]">რას ელოდები?*</h2>
+              <div class="pl-5 pb-[16px]">
+                <Field
+                  name="i_am_waiting"
+                  type="radio"
+                  value="registered_and_waiting"
+                  rules="required"
+                  v-model="i_am_waiting"
+                />
+                <label class="text-[20px] pl-[19px]"
+                  >დარეგისტრირებული ვარ და ველოდები რიცხვს</label
+                >
+              </div>
+              <div class="pl-5 pb-[16px]">
+                <Field
+                  name="i_am_waiting"
+                  type="radio"
+                  value="not_planning"
+                  v-model="i_am_waiting"
+                />
+                <label class="text-[20px] pl-[19px]">არ ვგეგმავ</label>
+              </div>
+              <div class="pl-5">
+                <Field
+                  name="i_am_waiting"
+                  type="radio"
+                  value="had_covid_and_planning_to_be_vaccinated"
+                  v-model="i_am_waiting"
+                />
+                <label class="text-[20px] pl-[19px]"
+                  >გადატანილი მაქვს და ვგეგმავ აცრას</label
+                >
+              </div>
+              <ErrorMessage
+                name="i_am_waiting"
+                class="text-[#F15524] text-base pt-[5px] pl-5"
+              />
+            </div>
+            <div
+              v-if="
+                values.i_am_waiting ===
+                'had_covid_and_planning_to_be_vaccinated'
+              "
+              class="pt-[39px] pl-[62px] text-xl"
+            >
+              <p>
+                ახალი პროტოკოლით კოვიდის გადატანიდან 1 <br />
+                თვის შემდეგ შეგიძლიათ ვაქცინის გაკეთება. <br />
+              </p>
+              <p class="pt-5">
+                👉 რეგისტრაციის ბმული <br />
+                <a
+                  href="https://booking.moh.gov.ge/"
+                  target="_blank"
+                  class="text-[#1289AE]"
+                >
+                  https://booking.moh.gov.ge/</a
+                >
+              </p>
+            </div>
           </div>
+
           <div><img src="@/assets/images/doctor2.png" alt="" /></div>
         </div>
         <div class="flex items-center justify-between m-auto max-w-[145px]">
@@ -60,10 +181,18 @@ export default {
     if (localStorage.had_vaccine) {
       this.had_vaccine = localStorage.had_vaccine;
     }
+    if (localStorage.vaccine_stage) {
+      this.vaccine_stage = localStorage.vaccination_stage;
+    }
+    if (localStorage.i_am_waiting) {
+      this.i_am_waiting = localStorage.i_am_waiting;
+    }
   },
   methods: {
     onSubmit() {
       this.$store.dispatch("vaccineModule/hadVaccine", this.had_vaccine);
+      this.$store.dispatch("vaccineModule/vaccineStage", this.vaccine_stage);
+      this.$store.dispatch("vaccineModule/waiting", this.i_am_waiting);
     },
   },
 };
--- src/store/covidVaccine/index.js
+++ src/store/covidVaccine/index.js
@@ -3,6 +3,8 @@ export default {
   state() {
     return {
       had_vaccine: null,
+      vaccination_stage: "",
+      i_am_waiting: "",
     };
   },
   mutations: {
@@ -14,15 +16,35 @@ export default {
       }
       localStorage.setItem("had_vaccine", payload);
     },
+    vaccineStage(state, payload) {
+      state.vaccination_stage = payload;
+      localStorage.setItem("vaccination_stage", payload);
+    },
+    waiting(state, payload) {
+      state.i_am_waiting = payload;
+      localStorage.setItem("i_am_waiting", payload);
+    },
   },
   actions: {
     hadVaccine(context, payload) {
       context.commit("hadVaccine", payload);
     },
+    vaccineStage(context, payload) {
+      context.commit("vaccineStage", payload);
+    },
+    waiting(context, payload) {
+      context.commit("waiting", payload);
+    },
   },
   getters: {
     hadVaccine(state) {
       return state.had_vaccine;
+    },
+    vaccineStage(state) {
+      return state.vaccination_stage;
+    },
+    waiting(state) {
+      return state.i_am_waiting;
     },
   },
 };
