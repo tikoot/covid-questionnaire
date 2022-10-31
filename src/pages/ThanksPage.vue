d79945381f46c3da6230d18fd081d0afbdafc8e8
tikoot <tiko.otarashvili99@gmail.com>
2022-10-31 14:44:43 +0100

    feat:add thanks page content

---
+++ src/pages/ThanksPage.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="bg-[#232323] w-full h-full flex items-center">
+    <h1 class="text-white text-[64px] font-bold font-contractica m-auto">
+      მ ა დ ლ ო ბ ა
+    </h1>
+  </div>
+</template>
